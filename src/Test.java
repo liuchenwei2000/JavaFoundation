f4dfe14b3c2bba62e7838d289596fa78942056c1
Vic Liu <liuchenwei2000@163.com>
2014-07-31 16:08:52 +0800

    1111111111

--- src/Test.java
+++ src/Test.java
@@ -17,7 +17,7 @@ public class Test {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("\"");
+		System.out.println(Integer.parseInt("-1000 0000 0000 0000 0000 0000 0000 0000", 2));
 	}
 
 }
