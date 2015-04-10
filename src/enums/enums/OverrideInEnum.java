9067ce21a900f15035e99ae8993cfe9e9ec9007f
Vic Liu <liuchenwei2000@163.com>
2015-04-10 10:47:34 +0800

    add enum part1

--- src/enums/enums/OverrideInEnum.java
+++ src/enums/enums/OverrideInEnum.java
@@ -5,8 +5,6 @@ package enums;
 
 /**
  * 覆盖enum中的方法
- * <p>
- * 实际上和覆盖普通类的方法没有什么区别。
  *
  * @author 刘晨伟
  *
@@ -14,13 +12,47 @@ package enums;
  */
 public class OverrideInEnum {
 
+	/**
+	 * 覆盖普通方法
+	 */
 	enum Sex {
 		MALE, FEMALE;
 
+		/**
+		 * 实际上和覆盖普通类的方法没有什么区别。
+		 * 
+		 * @see java.lang.Enum#toString()
+		 */
 		@Override
 		public String toString() {
 			return name().toLowerCase();
 		}
+	}
+	
+	/**
+	 * 可以为 enum 实例编写方法，从而为每个enum 实例赋予各自不同的行为，要实现常量相关的方法，
+	 * 需要为 enum 定义一个或多个 abstract 方法，然后每个 enum 实例实现该抽象方法。
+	 */
+	enum Signal {
+
+		ON() {
+
+			@Override
+			String getDescription() {
+				return "ON";
+			}
+
+		},
+		
+		OFF() {
+
+			@Override
+			String getDescription() {
+				return "OFF";
+			}
+		};
+
+		abstract String getDescription();
 	}
 
 	/**
@@ -29,6 +61,10 @@ public class OverrideInEnum {
 	public static void main(String[] args) {
 		for (Sex sex : Sex.values()) {
 			System.out.println(sex);
+		}
+		
+		for (Signal signal : Signal.values()) {
+			System.out.println(signal.getDescription());
 		}
 	}
 }
